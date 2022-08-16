d847dcee332ef4cd219d6e129bd606cf502de09d
cesartidrick <cesartidrick@gmail.com>
2022-08-15 18:42:35 -0600

    commit tree 9a3051ea3d5df669d1590c81ec7ed02c6b3329bc

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is Arel."
+end
+
+def batch_badge_creator(attendees)
+    badges = []
+    attendees.each do |attendee|
+        badges << "Hello, my name is #{attendee}."
+    end
+    badges
+end
+
+def assign_rooms(attendees)
+    rooms = []
+    attendees.each_with_index do |attendee, idx|
+        rooms << "Hello, #{attendee}! You'll be assigned to room #{idx + 1 }!"
+    end
+    rooms
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(attendees).each do |assignment|
+        puts assignment
+    end
+end
